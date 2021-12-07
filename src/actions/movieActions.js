81124bb545c7ebcd4e44e2d34fb6df1c975d77a4
sagun-shrestha <sshrestha426699@gmail.com>
2021-12-07 16:39:48 -0700

    almost done

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,5 +1,17 @@
 export const DELETE_MOVIE = "DELETE_MOVIE";
+export const ADD_MOVIE = "ADD_MOVIE";
 
 export const deleteMovie = (id)=>{
     return({type: DELETE_MOVIE, payload:id});
+}
+
+export const addMovie = (info) => {
+    return({type: ADD_MOVIE, payload:{
+        id:date.now(),
+        title: info.title,
+        director: info.director,
+        genre: info.genre,
+        metascore: parseInt(info.metascore),
+        description:info.description
+    }})
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
+import favoriteReducer from './favoritesReducer';
 
-export default movieReducer;
+const rootReducer = combineReducers({
+    movieReducer: movieReducer,
+    favoriteReducer: favoriteReducer
+
+})
+
+export default rootReducer;
